74455b8026ed4ea10e2df227324d2d6889dc10e2
arzumanyasha <arzumanianartur0@gmail.com>
2018-09-22 03:03:22 +0300

    msg2

---
+++ app/src/main/java/com/example/user/vandrova/FeedbackBottomSheetDialog.java
@@ -0,0 +1,57 @@
+package com.example.user.vandrova;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FeedbackBottomSheetDialog extends BottomSheetDialogFragment {
+    private BottomSheetListener sheetListener;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        Button leaveFeedback;
+        Button viewFeedbacks;
+        leaveFeedback = v.findViewById(R.id.feedbackBtn);
+        viewFeedbacks = v.findViewById(R.id.ratingListBtn);
+        leaveFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sheetListener.onButtonClicked(1);
+                dismiss();
+            }
+        });
+
+        viewFeedbacks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sheetListener.onButtonClicked(2);
+                dismiss();
+            }
+        });
+        return v;
+    }
+
+    public interface BottomSheetListener {
+        void onButtonClicked(int btnNumb);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            sheetListener = (BottomSheetListener) context;
+        }catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement buttom sheet listener");
+        }
+
+    }
+}
